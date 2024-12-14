36f5400374b96b4873cd79cd426abb70f37b0895
Yume_Mirai <asfari160904@gmail.com>
2024-12-14 11:56:32 +0700

    Add addition function in fitur-penjumlahan branch

--- main.c
+++ main.c
@@ -1,7 +1,13 @@
 // main.c
 #include <stdio.h>
 
+// Fungsi penjumlahan
+int tambah(int a, int b) {
+    return a + b;
+}
+
 int main() {
-    printf("Hello from main branch!\n");
+    printf("Hello from fitur-penjumlahan branch!\n");
+    printf("5 + 3 = %d\n", tambah(5, 3));
     return 0;
 }
